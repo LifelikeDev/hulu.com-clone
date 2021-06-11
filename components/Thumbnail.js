0f8d9c5ce8b569dfcf80ab52e888c97d1b49757c
Navian <primeguyy@gmail.com>
2021-06-11 08:18:28 +0000

    styles of image thumbnails added

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import { ThumbUpIcon } from "@heroicons/react/outline";
+import { ThumbUpIcon } from "@heroicons/react/solid";
 
 const Thumbnail = ({ result }) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
-    <article>
+    <article className="group cursor-pointer p-2 transition duration-150 ease-in transform sm:hover:scale-105 hover:z-50">
       <Image
         layout="responsive"
         src={
@@ -17,8 +17,12 @@ const Thumbnail = ({ result }) => {
 
       <div className="p-2">
         <p className="truncate max-w-md">{result.overview}</p>
-        <h2>{result.title || result.original_name}</h2>
-        <p>
+
+        <h2 className="text-white my-1 text-xl transition-all duration-100 ease-in-out group-hover:font-semibold">
+          {result.title || result.original_name}
+        </h2>
+
+        <p className="flex items-center opacity-0 group-hover:opacity-100">
           {result.media_type && `${result.media_type} •`}{" "}
           {result.release_date || result.first_air_date} •{" "}
           <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
